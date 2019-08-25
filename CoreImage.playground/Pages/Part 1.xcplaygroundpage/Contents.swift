b0b1748efe94a6d8e3dd4c08b5c502de80f205fe
Sam Davies <sam@razeware.com>
2019-08-24 20:56:49 -0600

    part01_10: Demonstrating how UIImageView can't resize a CIImage

--- CoreImage.playground/Pages/Part 1.xcplaygroundpage/Contents.swift
+++ CoreImage.playground/Pages/Part 1.xcplaygroundpage/Contents.swift
@@ -10,35 +10,39 @@ let depthMap = CIImage(contentsOf: url, options: [
   CIImageOption.auxiliaryDepth: true
 ])!
 
-let vibranceFilter = CIFilter(name: "CIVibrance", parameters: ["inputAmount": 0.9, "inputImage": image])!
-let gloomFilter = CIFilter(name: "CIGloom", parameters: ["inputRadius": 10, "inputIntensity": 0.7, "inputImage": vibranceFilter.outputImage!])!
-
-let output = gloomFilter.outputImage!.cropped(to: image.extent)
-
-timeCode {
-  let image1 = vibrance(image)
-  UIImageView(image: UIImage(ciImage: image1))
-  let image2 = gloom(image1).cropped(to: image.extent)
-  UIImageView(image: UIImage(ciImage: image2))
-  let image3 = vignette(image2)
-  UIImageView(image: UIImage(ciImage: image3))
-}
-
-timeCode {
-  let out = filterChain(image)
-  UIImageView(image: UIImage(ciImage: out))
-}
-
-
+//let vibranceFilter = CIFilter(name: "CIVibrance", parameters: ["inputAmount": 0.9, "inputImage": image])!
+//let gloomFilter = CIFilter(name: "CIGloom", parameters: ["inputRadius": 10, "inputIntensity": 0.7, "inputImage": vibranceFilter.outputImage!])!
+//
+//let output = gloomFilter.outputImage!.cropped(to: image.extent)
+//
+//timeCode {
+//  let image1 = vibrance(image)
+//  UIImageView(image: UIImage(ciImage: image1))
+//  let image2 = gloom(image1).cropped(to: image.extent)
+//  UIImageView(image: UIImage(ciImage: image2))
+//  let image3 = vignette(image2)
+//  UIImageView(image: UIImage(ciImage: image3))
+//}
+//
+//timeCode {
+//  let out = filterChain(image)
+//  UIImageView(image: UIImage(ciImage: out))
+//}
 
 
+
+let output = filterChain(image)
 
 
 
 let uiImage = UIImage(ciImage: output)
-let iv = UIImageView(image: uiImage)
-PlaygroundPage.current.liveView = iv
-PlaygroundPage.current.needsIndefiniteExecution = true
+let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+iv.contentMode = .scaleAspectFit
+iv.image = uiImage
+
+
+
+
 
 
 //: [Next](@next)
